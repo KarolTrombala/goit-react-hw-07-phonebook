a4be41de37f879cc0dc2a122418d974ed666089e
Karol Trombala <ktrombala@gmail.com>
2024-03-15 11:54:35 +0100

    update

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,9 @@
-import { createSelector } from '@reduxjs/toolkit'
+import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContacts = (state) => state.contacts.contacts
-export const selectFilter = (state) => state.filter
-export const selectIsLoading = (state) => state.contacts.isLoading
-export const selectError = (state) => state.contacts.error
+export const selectContacts = (state) => state.contacts.contacts;
+export const selectFilter = (state) => state.filter;
+export const selectIsLoading = (state) => state.contacts.isLoading;
+export const selectError = (state) => state.contacts.error;
 
 export const selectVisibleContacts = createSelector(
     [selectContacts, selectFilter],
