3e77789f6212d81d50b326d0dfce91216ec0a5e7
Karol Trombala <ktrombala@gmail.com>
2024-03-15 11:48:04 +0100

    app update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,22 @@
-// import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+
+import { fetchContacts } from 'redux/operations'
+import { selectError, selectIsLoading } from 'redux/selectors'
+
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { fetchContacts } from 'redux/operations';
-import { selectError, selectIsLoading } from 'redux/selectors';
+
 
 export const App = () => {
-  const dispatch = useDispatch()
-  const isLoading = useSelector(selectIsLoading)
-  const error = useSelector(selectError)
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
-    dispatch(fetchContacts())
-  }, [dispatch])
+    dispatch(fetchContacts());
+  }, [dispatch]);
   
   
   return (
@@ -36,6 +38,7 @@ export const App = () => {
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
+      {error && <p>Error, could not load.</p>}
     </div>
   )
 };
